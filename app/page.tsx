b56d3e17b8cfc85b6d4a0e0b44983a9e3a152e46
VillageR88 <villager88pl@gmail.com>
2024-06-09 11:21:19 +0200

    circle improved visually
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
     <main className="group/home relative z-0 flex h-dvh flex-col items-center overflow-x-clip sm:min-h-screen screen840:justify-center screen840:px-6">
-      <div className="flex size-[410px] items-center justify-center rounded-full bg-gradient-to-br from-[#0E112A] to-[#2E325A]">
-        <div className="flex size-[366px] flex-col items-center justify-center rounded-full bg-[#161932]">
-          <svg viewBox="0 0 36 36">
+      <div className="relative flex size-[410px] items-center justify-center rounded-full bg-gradient-to-br from-[#0E112A] to-[#2E325A]">
+        <div className="absolute size-full rounded-full shadow-[55px_45px_60px_-15px_rgba(18,21,48,1)]"></div>
+        <div className="absolute size-full rounded-full shadow-[-55px_-45px_60px_-15px_rgba(39,44,90,1)]"></div>
+        <div className="relative flex size-[366px] flex-col items-center justify-center rounded-full bg-[#161932]">
+          <svg className="absolute" viewBox="0 0 36 36">
             <path
               className="circle"
               stroke-dasharray="60"
@@ -12,6 +14,8 @@ export default function Home() {
       a 15.9155 15.9155 0 0 1 0 -31.831"
             />
           </svg>
+          <div className="absolute font-kumbhSans text-[100px] font-bold tracking-[-5px] text-[#D7E0FF]">17:59</div>
+          <div className="absolute mt-[170px] text-[16px] font-bold tracking-[15px] text-[#D7E0FF]">PAUSE</div>
         </div>
       </div>
     </main>
