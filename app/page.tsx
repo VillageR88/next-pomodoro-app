7cd937d37390ee8cce5d6ae76616352ae72e229d
VillageR88 <villager88pl@gmail.com>
2024-06-09 10:25:17 +0200

    initial line in circle
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,17 @@ export default function Home() {
   return (
     <main className="group/home relative z-0 flex h-dvh flex-col items-center overflow-x-clip sm:min-h-screen screen840:justify-center screen840:px-6">
       <div className="flex size-[410px] items-center justify-center rounded-full bg-gradient-to-br from-[#0E112A] to-[#2E325A]">
-        <div className="size-[366px] rounded-full bg-[#161932]"></div>
+        <div className="flex size-[366px] flex-col items-center justify-center rounded-full bg-[#161932]">
+          <svg viewBox="0 0 36 36">
+            <path
+              className="circle"
+              stroke-dasharray="60"
+              d="M18 2.0845
+      a 15.9155 15.9155 0 0 1 0 31.831
+      a 15.9155 15.9155 0 0 1 0 -31.831"
+            />
+          </svg>
+        </div>
       </div>
     </main>
   );
