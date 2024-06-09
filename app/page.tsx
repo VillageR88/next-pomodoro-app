3cba02a3b1989a081c36599e2db7f71be400dbe9
VillageR88 <villager88pl@gmail.com>
2024-06-09 10:09:27 +0200

    inner inner circle
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main className="group/home relative z-0 flex h-dvh flex-col items-center overflow-x-clip sm:min-h-screen screen840:justify-center screen840:px-6">
-      <div className="size-[410px] rounded-full bg-gradient-to-br from-[#0E112A] to-[#2E325A]"></div>
+      <div className="flex size-[410px] items-center justify-center rounded-full bg-gradient-to-br from-[#0E112A] to-[#2E325A]">
+        <div className="size-[366px] rounded-full bg-[#161932]"></div>
+      </div>
     </main>
   );
 }
