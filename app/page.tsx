a4f2fb9a297b8f45fa32592f5abfd3f94bbd7836
VillageR88 <villager88pl@gmail.com>
2024-06-09 11:32:03 +0200

    navbar initial
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
-    <main className="group/home relative z-0 flex h-dvh flex-col items-center overflow-x-clip sm:min-h-screen screen840:justify-center screen840:px-6">
-      <div className="relative flex size-[410px] items-center justify-center rounded-full bg-gradient-to-br from-[#0E112A] to-[#2E325A]">
+    <div className="group/home relative z-0 flex h-dvh flex-col items-center overflow-x-clip sm:min-h-screen screen840:justify-center screen840:px-6">
+      <nav className="z-10 flex h-[63px] w-[373px] items-center rounded-[31.5px] bg-[#161932] pl-[7px] font-kumbhSans text-[14px] font-bold">
+        <button className="h-[48px] w-[120px] rounded-[26.5px] bg-[#F87070] text-[#1E213F]" type="button">
+          pomodoro
+        </button>
+        <button type="button" className="ml-[22px] text-[#D7E0FF]/40">
+          short break
+        </button>
+        <button type="button" className="ml-[44px] text-[#D7E0FF]/40">
+          long break
+        </button>
+      </nav>
+      <main className="relative mt-[45px] flex size-[410px] items-center justify-center rounded-full bg-gradient-to-br from-[#0E112A] to-[#2E325A]">
         <div className="absolute size-full rounded-full shadow-[55px_45px_60px_-15px_rgba(18,21,48,1)]"></div>
         <div className="absolute size-full rounded-full shadow-[-55px_-45px_60px_-15px_rgba(39,44,90,1)]"></div>
         <div className="relative flex size-[366px] flex-col items-center justify-center rounded-full bg-[#161932]">
@@ -17,7 +28,7 @@ export default function Home() {
           <div className="absolute font-kumbhSans text-[100px] font-bold tracking-[-5px] text-[#D7E0FF]">17:59</div>
           <div className="absolute mt-[170px] text-[16px] font-bold tracking-[15px] text-[#D7E0FF]">PAUSE</div>
         </div>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }
