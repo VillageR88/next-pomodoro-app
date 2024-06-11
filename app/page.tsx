45ff3f9b11c1f6bee60d39412590b6f9f5560dec
VillageR88 <villager88pl@gmail.com>
2024-06-11 14:15:25 +0200

    padding correction as design
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Footer from './home/Footer';
 export default function Home() {
   return (
     <div
-      className={` group/home relative z-0 flex min-h-dvh flex-col items-center justify-center overflow-x-clip px-2 py-[32px] sm:min-h-screen sm:py-[80px] screen840:px-6`}
+      className={` group/home relative z-0 flex min-h-dvh flex-col items-center justify-center overflow-x-clip px-2 py-[32px] sm:min-h-screen sm:py-[48px] screen840:px-6`}
     >
       <Settings />
       <Header />
