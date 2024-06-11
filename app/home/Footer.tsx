5c55df62734c8b5e775e3dd435378b5037915dd2
VillageR88 <villager88pl@gmail.com>
2024-06-11 20:04:35 +0200

    footer show settings disabled when open
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/home/Footer.tsx
+++ app/home/Footer.tsx
@@ -6,10 +6,16 @@ import { DataContext } from '../_providers/DataContext';
 import { useContext } from 'react';
 
 export default function Footer() {
-  const { handleOpenSettings } = useContext(DataContext);
+  const { handleOpenSettings, showSettings } = useContext(DataContext);
   return (
     <footer className="mt-[63px]">
-      <button onClick={handleOpenSettings} title="settings" className="relative h-[28px] w-[27px]" type="button">
+      <button
+        disabled={showSettings}
+        onClick={handleOpenSettings}
+        title="settings"
+        className="relative h-[28px] w-[27px]"
+        type="button"
+      >
         <Image fill src={imageSettings as string} alt="settings" />
       </button>
     </footer>
